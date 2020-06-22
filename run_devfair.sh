99e08ceb935a0b6b9cc80546d8d8497ca85af1dd
Tianyu Li <tianyul@fb.com>
2020-06-22 15:36:38 -0700

    update run

--- run_devfair.sh
+++ run_devfair.sh
@@ -1,7 +1,7 @@
 module load anaconda3
 source activate locomotion-env
 
-for i in {4..10}
+for i in {11..20}
 do  
-	python ./experiment/run_experiment.py seed=$i reward.reward_function.rewards=[$i\*0.1,0.1,0.01] 
+	python ./experiment/run_experiment.py seed=$i reward.reward_function.rewards=[$i\*0.1,0.1,0.001] 
 done
