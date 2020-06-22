9255c7680f1d02d263af12ec068beed66b922f7b
Tianyu Li <tianyul@fb.com>
2020-06-21 18:32:52 -0700

    fixed bug

--- run_devfair.sh
+++ run_devfair.sh
@@ -3,5 +3,5 @@ source activate locomotion-env
 
 for i in {4..10}
 do  
-	python ./experiment/run_experiment.py seed=i reward.reward_function.rewards=[i\*0.1,0.1,0.01] 
+	python ./experiment/run_experiment.py seed=$i reward.reward_function.rewards=[$i\*0.1,0.1,0.01] 
 done
